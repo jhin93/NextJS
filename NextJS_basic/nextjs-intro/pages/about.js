e23a7cb618071a4735521357da57b8a1de006084
jhin93 <scene1993@gmail.com>
2022-06-14 17:56:49 +0900

    framework가 개발자의 코드를 불러서 사용, 개발자는 library를 불러서 사용

---
+++ NextJS_basic/nextjs-intro/pages/about.js
@@ -0,0 +1,7 @@
+export default function Potato(){
+    return "about us"
+}
+
+// react.js는 우리가 원할때 부르고 원할때 사용하는 library
+// framework는 내가 코드를 다루는 규칙이 있는 곳
+// 나의 코드가 framework라는 집의 게스트. framework가 코드를 불러서 사용한다.
