b0d4192724fb4e20279eb2c5290c5bdbf313b876
jhin93 <scene1993@gmail.com>
2022-06-14 18:54:31 +0900

    nextjs는 react와 달리<div id="root">가 아닌 html을 보여준다.

--- NextJS_basic/nextjs-intro/pages/index.js
+++ NextJS_basic/nextjs-intro/pages/index.js
@@ -1,9 +1,14 @@
+import {useState} from "react";
+
 export default function Home() {
+    const [counter, setCounter] = useState(0);
     return (
         <div>
-            <h1>
-                Hello
-            </h1>
+            <h1>Hello {counter}</h1>
+            <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
         </div>
     );
 }
+
+// nextjs는 react와 달리<div id="root">가 아닌 html을 보여준다. react가 <div id="root">를 풀어헤칠때까지 기다릴 필요가 없다는 것
+// 그리고 react가 발동되서 로직들이 작동한다.
