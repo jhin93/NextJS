cc5d14cd4be663cf12e5a6ba3e7e59808c3753ba
jhin93 <scene1993@gmail.com>
2022-06-14 21:24:43 +0900

    <styled jsx>{``}

--- NextJS_basic/nextjs-intro/components/NavBar.js
+++ NextJS_basic/nextjs-intro/components/NavBar.js
@@ -8,10 +8,10 @@ export default function NavBar() {
             {/* Link는 Nextjs에서 href를 사용할 수 있게 하는 규칙이다. */}
             {/* router에 훅을 걸어서 사용할 수 있다. router.pathname 이런 식으로. router의 속성은 console.log로 확인 가능*/}
             <Link href="/"> 
-                <a>Home</a> 
+                <a className={router.pathname === "/" ? "active" : ""}>Home</a> 
             </Link>
             <Link href="/about">
-                <a>About</a>
+                <a className={router.pathname === "/about" ? "active" : ""}>About</a>
             </Link>
             <style jsx>{`
                 nav {
@@ -19,6 +19,11 @@ export default function NavBar() {
                 }
                 a {
                     text-decoration : none;
+                }
+                {/* styled jsx는 컴포넌트로 범위를 한정시켜 클래스네임중복을 가능하게 해준다.
+                아래 active를 index.js의 h1에 사용해도 h1이 노란색으로 변하지 않는다. */}
+                .active {
+                    color : yellow;
                 }
             `}</style>
         </nav>
