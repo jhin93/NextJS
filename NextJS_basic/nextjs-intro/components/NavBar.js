b9c0bf3c35f1bed1a8c94474cd4a0af459ac4dd8
jhin93 <scene1993@gmail.com>
2022-06-14 19:20:38 +0900

    주석수정

--- NextJS_basic/nextjs-intro/components/NavBar.js
+++ NextJS_basic/nextjs-intro/components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
     const router = useRouter();
     return (
         <nav>
-            {/* Link는 a태그의 대체제이다. href를 쓸때 사용한다. */}
+            {/* Link는 Nextjs에서 href를 사용할 수 있게 하는 규칙이다. */}
             {/* router에 훅을 걸어서 사용할 수 있다. router.pathname 이런 식으로. router의 속성은 console.log로 확인 가능*/}
             <Link href="/"> 
                 <a style={{color: router.pathname === "/" ? "red" : "blue"}}>Home</a> 
