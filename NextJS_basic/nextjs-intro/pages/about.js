c6328bde87ae2d6065c3cf4715b04e23ff06fe5b
jhin93 <scene1993@gmail.com>
2022-06-14 18:18:15 +0900

    react를 import할 필요가 없다(메소드 ex useState 사용할땐 필요)

--- NextJS_basic/nextjs-intro/pages/about.js
+++ NextJS_basic/nextjs-intro/pages/about.js
@@ -1,7 +1,10 @@
-export default function Potato(){
+export default function Aboutus(){
     return "about us"
 }
 
 // react.js는 우리가 원할때 부르고 원할때 사용하는 library
 // framework는 내가 코드를 다루는 규칙이 있는 곳
 // 나의 코드가 framework라는 집의 게스트. framework가 코드를 불러서 사용한다.
+
+// index.js 는 기본 홈페이지
+// export default를 해줘야 한다.
