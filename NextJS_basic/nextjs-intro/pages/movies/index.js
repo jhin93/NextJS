35b68b6857cb503e58ab5343d1c5d674cc89d18a
jhin93 <scene1993@gmail.com>
2022-06-15 14:44:59 +0900

    url을 추가하는 방법

---
+++ NextJS_basic/nextjs-intro/pages/movies/index.js
@@ -0,0 +1,7 @@
+export default function All() {
+    return "movies index"
+}
+
+
+// /movies 라는 url을 만들고 싶다면 pages에 movies 폴더를 만들고, index.js를 만들면 됨
+// /movies/all 라는 url을 원한다면, movies안에 all.js를 작성한다
