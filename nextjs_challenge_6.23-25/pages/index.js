ec8d2887175bea12b53f11b9319857bfeb6f8b5d
jhin93 <scene1993@gmail.com>
2022-06-23 20:21:19 +0900

    챌린지 시작

---
+++ nextjs_challenge_6.23-25/pages/index.js
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Seo from "../components/Seo";
+
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, person) => {
+    router.push(`/billionare/${person}/${id}`);
+  };
+  console.log("------------results -------------", results);
+  return <div>test22</div>;
+}
+
+export async function getServerSideProps() {
+  const results = await (
+    await fetch(`https://billions-api.nomadcoders.workers.dev/`)
+  ).json();
+  return {
+    props: {
+      results
+    }
+  };
+}
