e26f23940496ec19b66558e24295ca03fbfe9de7
jhin93 <scene1993@gmail.com>
2022-06-27 19:50:40 +0900

    배경 이미지 삽입

--- carrot_market/carrot-market/pages/index.tsx
+++ carrot_market/carrot-market/pages/index.tsx
@@ -5,7 +5,8 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (    
-    <div className="dark:md:hover:bg-teal-400 bg-[url('/vercel.svg')]">
+    // 배경 이미지 삽입
+    <div className="dark:md:hover:bg-teal-400 bg-[url('/vercel.svg')]"> 
       <h2 className="text-[200px] text-[#000]">
         Hello
       </h2>
