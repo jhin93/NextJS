994aa71612ded48fddb67be906cf693e564642d0
jhin93 <scene1993@gmail.com>
2022-06-27 18:42:38 +0900

    tailwind modifier 중첩

--- carrot_market/carrot-market/pages/index.tsx
+++ carrot_market/carrot-market/pages/index.tsx
@@ -5,13 +5,10 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-  <div className = "flex flex-col space-y-2 p-5">
-    <ul className='list-disc marker:text-teal-500'>
-      <li>hi</li>
-      <li>hi</li>
-      <li>hi</li>
-    </ul>
-  </div>)
+    <div>
+
+    </div>
+  )
 };
 
 export default Home
