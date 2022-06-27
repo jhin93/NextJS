8b0c66599976c17976873eb6e8d7d80303e96e97
jhin93 <scene1993@gmail.com>
2022-06-27 18:38:07 +0900

    점 리스트

--- carrot_market/carrot-market/pages/index.tsx
+++ carrot_market/carrot-market/pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
   <div className = "flex flex-col space-y-2 p-5">
-    <ul className='list-decimal'>
+    <ul className='list-disc marker:text-teal-500'>
       <li>hi</li>
       <li>hi</li>
       <li>hi</li>
