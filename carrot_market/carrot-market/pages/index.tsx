05e555f70dafc8d632dbaa72e71feebd5840fef3
jhin93 <scene1993@gmail.com>
2022-06-29 09:06:21 +0900

    5.1 소스코드

--- carrot_market/carrot-market/pages/index.tsx
+++ carrot_market/carrot-market/pages/index.tsx
@@ -4,14 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (    
-    // 배경 이미지 삽입
-    <div className="dark:md:hover:bg-teal-400 bg-[url('/vercel.svg')]"> 
-      <h2 className="text-[200px] text-[#000]">
-        Hello
-      </h2>
-    </div>
-  )
+  return null;
 };
 
 export default Home
