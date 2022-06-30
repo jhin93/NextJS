d42634d6956fbe3cf310d11194532af522797782
jhin93 <scene1993@gmail.com>
2022-06-30 16:50:29 +0900

    5.6 완료

--- carrot_market/pages/enter.tsx
+++ carrot_market/pages/enter.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-
 function cls(...classnames: string[]) {
   return classnames.join(" ");
 }
-
 const Enter: NextPage = () => {
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => setMethod("email");
@@ -41,13 +39,14 @@ const Enter: NextPage = () => {
           </div>
         </div>
         <form className="flex flex-col mt-8">
-          <label className="text-sm font-medium text-gray-700">
+          <label htmlFor="input" className="text-sm font-medium text-gray-700">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className="mt-1">
             {method === "email" ? (
               <input
+                id="input"
                 type="email"
                 className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                 required
@@ -59,6 +58,7 @@ const Enter: NextPage = () => {
                   +82
                 </span>
                 <input
+                  id="input"
                   type="number"
                   className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                   required
@@ -82,10 +82,9 @@ const Enter: NextPage = () => {
           </div>
           <div className="grid grid-cols-2 mt-2 gap-3">
             <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-            <svg
+              <svg
                 className="w-5 h-5"
                 aria-hidden="true"
-
                 fill="currentColor"
                 viewBox="0 0 20 20"
               >
@@ -96,7 +95,6 @@ const Enter: NextPage = () => {
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
-
                 fill="currentColor"
                 viewBox="0 0 20 20"
               >
