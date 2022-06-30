d2774f291148ea8cd0d0efc8c19d31042f5afe9f
jhin93 <scene1993@gmail.com>
2022-06-30 16:53:20 +0900

    5.7 완료

--- carrot_market/pages/items/upload.tsx
+++ carrot_market/pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
+    <div className="px-4 py-10">
       <div>
         <label className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
           <svg
@@ -48,7 +48,6 @@ const Upload: NextPage = () => {
         <label className="mb-1 block text-sm font-medium text-gray-700">
           Description
         </label>
-
         <textarea
           className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
           rows={4}
