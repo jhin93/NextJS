d19a2853896ae3019f5fbdd376844236eb3ec3b9
jhin93 <scene1993@gmail.com>
2022-06-30 16:17:32 +0900

    2.0 완료

--- carrot_market/pages/index.tsx
+++ carrot_market/pages/index.tsx
@@ -2,10 +2,9 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
+    <div className="bg-red-500 text-center flex">
       <h1 className="text-black">it works</h1>
     </div>
   );
 };
-
 export default Home;
