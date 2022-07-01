941b3497493b52f278959f40d7cdab8ec364e262
jhin93 <scene1993@gmail.com>
2022-07-01 10:42:16 +0900

    npm install react-hook-form

--- carrot_market/pages/api/client-test.tsx
+++ carrot_market/pages/api/client-test.tsx
@@ -5,7 +5,6 @@ import client from "../../libs/client";
 export default async function handler(
     req:NextApiRequest, res:NextApiResponse
 ) {
-
     await client.user.create({data: {
         email: "hi",
         name: "hi"
@@ -17,4 +16,4 @@ export default async function handler(
 }
 
 // http://localhost:3000/api/client-test에서 확인 가능
-// api 라우트를 만드는 방식 api 폴더 생성 후 파일 생성
+// api 라우트를 만드는 방식 : api 폴더 생성 후 파일 생성
