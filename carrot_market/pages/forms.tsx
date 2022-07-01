394ce60f3e3d5a99b475e1c015a3c48a90008ca7
jhin93 <scene1993@gmail.com>
2022-07-01 11:53:14 +0900

    useForm, register, watch

--- carrot_market/pages/forms.tsx
+++ carrot_market/pages/forms.tsx
@@ -1,43 +1,24 @@
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+// 해결 Less code 
+// Better validation
+// Better Errors (set, clear, display)
+// Have control over inputs
+// 해결 Don't deal with events
+// 해결 Easier Inputs
 
 export default function Forms() {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [formErrors, setFormErrors] = useState("");
-
-    const onUsernameChange = (event:React.SyntheticEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: {value},
-        } = event;
-        setUsername(value);
-    } 
-
-    const onEmailChange = (event:React.SyntheticEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: {value},
-        } = event;
-        setEmail(value);
-    } 
-
-    const onPasswordChange = (event:React.SyntheticEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: {value},
-        } = event;
-        setPassword(value);
-    } 
-    const onSubmit = (event:React.SyntheticEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if(username === "" || email === "" || password === ""){
-            setFormErrors("All fields are required")
-        }
-    }
-    console.log(username, email, password);
+    // 모든건 useForm으로부터 나온다.
+    // input들을 전부 state에 '등록'하기 위해선 register 함수를 사용한다
+    // watch : input에 입력한 값을 브라우저에서 확인할 수 있다.
+    const {register, watch} = useForm();
+    console.log(watch())
     return (
-        <form onSubmit={onSubmit}>
-            <input value={username} onChange={onUsernameChange} type="text" placeholder="Username" required minLength={5}/>
-            <input value={email} onChange={onEmailChange} type="email" placeholder="email" required/>
-            <input value={password} onChange={onPasswordChange} type="password" placeholder="password" required/>
+        <form>
+            {/* ...register가 input의 이름을 인수로 받고, onChange, eventListner, value.. 등의 속성을 알아서 처리해준다. */}
+            <input {...register("username")} type="text" placeholder="Username" required minLength={5}/> 
+            <input {...register("email")} type="email" placeholder="email" required/>
+            <input {...register("password")} type="password" placeholder="password" required/>
             <input type="submit" value="Create Account" />
         </form>
     )
