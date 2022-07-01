9c0104d4a6ea62a1f596941c9db87edc2b4710cc
jhin93 <scene1993@gmail.com>
2022-07-01 18:10:54 +0900

    setError 사용 예시

--- carrot_market/pages/forms.tsx
+++ carrot_market/pages/forms.tsx
@@ -8,20 +8,28 @@ interface LoginForm {
     username: string;
     password: string;
     email: string;
+    errors?: string;
 }
 
 export default function Forms() {
-    const {register, handleSubmit, formState : { errors }} = useForm<LoginForm>({
+    const {
+        register, 
+        handleSubmit, 
+        watch, // 하나의 필드를 관찰 가능
+        formState : { errors }, // 에러를 객체로 만드는 메소드. 이게 있어야 setError 등 에러 관련 메소드를 쓸 수 있다.
+        setError, // 에러를 의도한대로 설정 가능. 특정한 필드에만 설정할 수도 있다.
+        setValue, // 값을 직접 수정하고 싶을 때 쓰는 메소드
+        reset // form을 초기화하는 메소드
+    } = useForm<LoginForm>({
         mode: "onChange"
     });
     const onValid = (data:LoginForm) => {
         console.log("i'm valid")
+        setError("username", {message: "Taken username"}) // setError로 username에만 에러를 설정한 모습
     }
     const onInvalid = (errors: FieldErrors) => {
         console.log(errors);
     }
-    // formState의 errors
-    console.log(errors) 
     return ( 
         <form onSubmit={handleSubmit(onValid, onInvalid)}>
             <input {...register("username", {
@@ -31,6 +39,7 @@ export default function Forms() {
                     value: 5 
                 } 
             })} type="text" placeholder="Username"/> 
+            {errors.username?.message}
             <input {...register("email", {
                 required: "Email is required",
                 validate: {
@@ -44,6 +53,7 @@ export default function Forms() {
                 required: "Password is required"
             })} type="password" placeholder="password"/>
             <input type="submit" value="Create Account" />
+            {errors.errors?.message} {/* formState의 에러 중 errors가 있다면 그것의 message를 브라우저에 찍는다<div className=""></div> */}
         </form>
     )
 }
