9a5952fde6c33f504c784a57a0e0023c8e160173
jhin93 <scene1993@gmail.com>
2022-07-04 19:17:53 +0900

    중간커밋

--- carrot_market/pages/api/users/enter.tsx
+++ carrot_market/pages/api/users/enter.tsx
@@ -1,18 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
+import withHandler from "../../../libs/server/withHandler";
 
 
-export default async function handler(
-    req:NextApiRequest, 
-    res:NextApiResponse
-) {
-    if (req.method !== "POST") {
-        res.status(401).end();
-      }
-      console.log(req.body);
-      res.status(200).end();
-    }
-
+async function handler(req:NextApiRequest, res:NextApiResponse) {
+    console.log(req.body);
+    res.status(200).end();
+}
 // http://localhost:3000/api/client-test에서 확인 가능
 // api 라우트를 만드는 방식 : api 폴더 생성 후 파일 생성
+
+// nextJS에서 api route를 만들때는 해당 function을 export default 해야 한다.
+export default withHandler("POST", handler) // withHandler를 호출하면, async function(req:NextApiRequest, res:NextApiResponse) {...} 가 리턴된다.
+
+
+
+
+
+
+
+
 
