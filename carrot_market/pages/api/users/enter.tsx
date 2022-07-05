f20b76ef5be3b2d385ead5bd6fbdbf71b6248dde
jhin93 <scene1993@gmail.com>
2022-07-05 13:56:18 +0900

    9.1 완료

--- carrot_market/pages/api/users/enter.tsx
+++ carrot_market/pages/api/users/enter.tsx
@@ -5,25 +5,39 @@ import client from "@libs/server/client";
 
 async function handler(req:NextApiRequest, res:NextApiResponse) { // handler 주변에 껍데기를 만듬(withHandler)
     const { phone, email } = req.body // 전화번호 혹은 이메일 둘중 하나를 가짐.
-    let user;
-    if(email){ // 만약 입력된 이메일의 유저를 찾는다면
-        const user = await client.user.findUnique({ // client로 prisma에 접근
-            where: {
-                email,
-            }
-        });
-        if(user) console.log("found it")
-        if(!user){ // 만약 찾는 유저가 없다면 create user.
-            console.log("Did not find. Will create")
-            await client.user.create({ // create 메소드는 다음과 같이 유저를 만든다 
-                data: { // 유저를 특정지을 수 있는 데이터를 같이 보내줘야 함. 필수로 보내야 하는 name을 보낸다. 전화번호 이메일은 없을수도 있음
-                    name:"Anonymous",
-                    email,
-                }
-            })
-        }
-        console.log(user)
-    }
+    const payload = phone ? { phone: +phone } : {email} // phone 혹은 email인 상황을 3항연산자로 해결
+    // upsert. prisma 메소드
+    // upsert는 뭔가 생성하거나 수정할 때 사용한다.
+    // .upsert()는 3가지 메소드를 포함한다. create 생성, update 수정, where 검색
+    const user = await client.user.upsert({
+        where : {
+            ...payload,
+        },
+        create: {
+            name: "Anonymous", // 에러 : 'name' 속성이 '{ phone: number; }' 형식에 없지만 'UserCreateInput' 형식에서 필수입니다.
+            ...payload,
+        },
+        update: {} // 이 경우엔 찾아도 update 안할 것이기에 비워둠.
+    })
+    console.log("user : ", user)
+    // if(email){ // 만약 입력된 이메일의 유저를 찾는다면
+    //     const user = await client.user.findUnique({ // client로 prisma에 접근
+    //         where: {
+    //             email,
+    //         }
+    //     });
+    //     if(user) console.log("found it")
+    //     if(!user){ // 만약 찾는 유저가 없다면 create user.
+    //         console.log("Did not find. Will create")
+    //         await client.user.create({ // create 메소드는 다음과 같이 유저를 만든다 
+    //             data: { // 유저를 특정지을 수 있는 데이터를 같이 보내줘야 함. 필수로 보내야 하는 name을 보낸다. 전화번호 이메일은 없을수도 있음
+    //                 name:"Anonymous",
+    //                 email,
+    //             }
+    //         })
+    //     }
+    //     console.log(user)
+    // }
     return res.status(200).end();
 }
 // http://localhost:3000/api/client-test에서 확인 가능
