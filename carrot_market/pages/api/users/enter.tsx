29cf4e6cc92b83564bf514dd09e177f799571e2e
jhin93 <scene1993@gmail.com>
2022-07-06 18:44:58 +0900

    9.9 완료 리다이렉트

--- carrot_market/pages/api/users/enter.tsx
+++ carrot_market/pages/api/users/enter.tsx
@@ -12,7 +12,7 @@ async function handler(
     res: NextApiResponse<ResponseType>
   ) {
     const { phone, email } = req.body;
-    const user = phone ? { phone: +phone } : email ? { email } : null; // 2중 3항 연산자. 폰 먼저 체크하고, 없으면 이메일 체크, 이메일도 없으면 null
+    const user = phone ? { phone } : email ? { email } : null; // 2중 3항 연산자. 폰 먼저 체크하고, 없으면 이메일 체크, 이메일도 없으면 null
     if (!user) return res.status(400).json({ ok: false }); // user가 null일때 400 에러
     const payload = Math.floor(100000 + Math.random() * 900000) + "";
     const token = await client.token.create({
