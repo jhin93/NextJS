34198fdbff936002082811d0762e2f29cb5159a0
jhin93 <scene1993@gmail.com>
2022-07-06 16:59:48 +0900

    9.7 완료

--- carrot_market/libs/server/withHandler.ts
+++ carrot_market/libs/server/withHandler.ts
@@ -14,7 +14,10 @@ export default function withHandler(
     // withHandler는 껍데기일 뿐이고, 아래 리턴되는 함수가 nextJS에 의해 실행된다. 우리가 원하는 대로 function을 설정해서 return 할 수 있다. 메소드와 fn 두 인자를 활용해서.
     // nextJS에서 api route를 만들때는 해당 function을 export default 해야 한다.
     // enter.tsx에서 삭제한 handler 함수를 리턴해야 하고, 그러려면 아래와 같이(handler 함수의 인자도 포함해서) 작성해야 한다.
-    return async function(req:NextApiRequest, res:NextApiResponse) {
+    return async function (
+        req: NextApiRequest,
+        res: NextApiResponse
+      ): Promise<any> {
         if(req.method !== method){ // req.method(들어온 method)가 우리가 원한 method(인자의 method 중 하나)가 아니라면
             res.status(405).end(); // bad request(405) 출력.
             return;
