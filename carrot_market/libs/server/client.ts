867603deb11ed33a21244b6add05259a86a318df
jhin93 <scene1993@gmail.com>
2022-07-07 11:28:23 +0900

    작업 루틴 model-database-mutation-useSWR로 데이터 가져오기

--- carrot_market/libs/server/client.ts
+++ carrot_market/libs/server/client.ts
@@ -1,3 +1,11 @@
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
- export default new PrismaClient();
+declare global {
+  var client: PrismaClient | undefined;
+}
+
+const client = global.client || new PrismaClient(); // 처음 구동될때는 새로 생성 = new
+
+if (process.env.NODE_ENV === "development") global.client = client;
+
+export default client;
